d818206e79f8046a803751b7a21aa0c594f00946
Shubham-2899 <shubhamlatake123@gmail.com>
2024-05-26 00:00:38 +0530

    added header with logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Container } from '@mui/material';
 import './App.css';
 import MyWorkRoutes from './routes';
 import MyTaskContext from './contexts/MyTaskContext';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Container maxWidth={false} sx={{ paddingTop: '10px' }}>
         <MyTaskContext>
-          <MyWorkRoutes />
+          <>
+            <Header />
+            <MyWorkRoutes />
+          </>
         </MyTaskContext>
       </Container>
     </>
---
+++ src/components/Header.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
+import mydailyworkIcon from '../assets/mydailywork.svg';
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Header() {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const navigate = useNavigate();
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleMenuItemClick = (setting: string) => {
+    handleCloseUserMenu();
+    switch (setting) {
+      case 'Profile':
+        navigate('/profile');
+        break;
+      case 'Account':
+        navigate('/account');
+        break;
+      case 'Dashboard':
+        navigate('/dashboard');
+        break;
+      case 'Logout':
+        // Handle logout logic here
+        navigate('/logout');
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <AppBar
+      position="static"
+      color="transparent"
+      elevation={0}
+      sx={{ borderBottom: '1px solid #ddd', backgroundColor: '#fff', mb: '20px' }}
+    >
+      <Toolbar
+        disableGutters
+        sx={{ display: 'flex', justifyContent: 'space-between', paddingLeft: '15px', paddingRight: '15px' }}
+      >
+        <img src={mydailyworkIcon} alt="my daily work" width={200} height={50} />
+
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={() => handleMenuItemClick(setting)}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
